cda872fa8596463814e7fa65c9d91ff8183248c3
eunveloper <hye_7991@naver.com>
2022-06-15 02:49:21 +0900

    Initial commit

---
+++ src/test/java/com/dynamic/query/querydsl/QuerydslApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dynamic.query.querydsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuerydslApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
